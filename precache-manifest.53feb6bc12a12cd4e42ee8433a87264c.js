ee0b3ad37004c60d9d7a1a952857481df105b18a
sturdy75 <sturdy75@gmail.com>
2019-05-01 09:44:10 -0600

    Updates

---
+++ precache-manifest.53feb6bc12a12cd4e42ee8433a87264c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560211e086c15214e9da4ecac44f371c",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "471d7316ea9ce6ce423a",
+    "url": "/ClickyGame/static/css/main.6f261858.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ClickyGame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "471d7316ea9ce6ce423a",
+    "url": "/ClickyGame/static/js/main.f5db7613.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "378a2f4b221c7fbbd1a676913405d93d",
+    "url": "/ClickyGame/static/media/bifur.378a2f4b.jpg"
+  },
+  {
+    "revision": "1d9e7f64e6684bf1135e1ea5a419d093",
+    "url": "/ClickyGame/static/media/bilbo.1d9e7f64.jpg"
+  },
+  {
+    "revision": "4db5f5a7537f6cd5cffee0aecdaabcd5",
+    "url": "/ClickyGame/static/media/bofur.4db5f5a7.jpg"
+  },
+  {
+    "revision": "c7f3c7d448ee0d327c3a59c3ef10bd0a",
+    "url": "/ClickyGame/static/media/elf.c7f3c7d4.jpg"
+  },
+  {
+    "revision": "d13842e74e0803f36b2fb2e573faf649",
+    "url": "/ClickyGame/static/media/fili.d13842e7.jpg"
+  },
+  {
+    "revision": "36c6130b63774cd651acdfad2619c714",
+    "url": "/ClickyGame/static/media/galadriel.36c6130b.jpg"
+  },
+  {
+    "revision": "bd0dc0bb9b72f023438af65f1c2609e2",
+    "url": "/ClickyGame/static/media/gandalf.bd0dc0bb.jpg"
+  },
+  {
+    "revision": "442b8d08b7096d33bc821fe87d6ddd1d",
+    "url": "/ClickyGame/static/media/gloin.442b8d08.jpg"
+  },
+  {
+    "revision": "829821e4570aa8d8f51bbda9fd37b39d",
+    "url": "/ClickyGame/static/media/gollum.829821e4.jpg"
+  },
+  {
+    "revision": "76f27ffa01d1d6b3ded74e85c3e147bd",
+    "url": "/ClickyGame/static/media/kili.76f27ffa.jpg"
+  },
+  {
+    "revision": "1b402fae0ef931902b0e1a5a116ee374",
+    "url": "/ClickyGame/static/media/nori.1b402fae.jpg"
+  },
+  {
+    "revision": "a1f4f3fab4d066529aca72d9b71bd29e",
+    "url": "/ClickyGame/static/media/ori.a1f4f3fa.jpg"
+  },
+  {
+    "revision": "084a093e0cad05ad6f7b8b4d7c67f67c",
+    "url": "/ClickyGame/static/media/thorin.084a093e.jpg"
+  }
+]);
